9408eb25c1b0c46ae65430f6194fd3ed67d08aba
yizhuoDi <119659063+yizhuoDi@users.noreply.github.com>
2023-07-07 04:48:42 +0800

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,11 @@
+export CUDA_VISIBLE_DEVICES=7
+python videosam.py  \
+  --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py \
+  --grounded_checkpoint groundingdino_swint_ogc.pth \
+  --sam_checkpoint sam_vit_h_4b8939.pth \
+  --input_image ../data/collision/ \
+  --output_dir "outputs" \
+  --box_threshold 0.3 \
+  --text_threshold 0.25 \
+  --text_prompt "ball" \
+  --device "cuda"
